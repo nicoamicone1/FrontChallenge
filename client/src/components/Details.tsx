a6b0af83dc45703eb6396a032316cf3ab6deea30
Nico <nicoamicone1@gmail.com>
2022-07-21 21:50:12 -0300

    gradients

--- client/src/components/Details.tsx
+++ client/src/components/Details.tsx
@@ -27,43 +27,55 @@ export default function ProductDetails() {
             name:'',
             logo_url:''}
     })
+    const [image,setImage]=useState<string>('')
 
     useEffect(()=>{
         window.scroll(0,0)
-        fetch(`http://localhost:3000/products/${id}`).then((res)=>res.json()).then(data=>setProduct(()=>data as Int.Product))
+        fetch(`http://localhost:3000/products/${id}`).then((res)=>res.json()).then(data=>{
+            setImage(()=>data.image_url[0])
+            setProduct(()=>data as Int.Product)
+        })
     },[])
   return (
     <Box>
         <NavBar/>
-        <Box sx={{mt:{xs:'250px',md:9},display:'flex',justifyContent:'center',height:'100vh'}}>
+        <Box sx={{mt:{xs:'250px',md:10},display:'flex',justifyContent:'center',height:'100vh'}}>
             <Box sx={{display:'flex',justifyContent:'center',maxHeight:508,flexDirection:{xs:'column',md:'row'}}}>
-                <Box sx={{display:'flex',flexDirection:'column',maxWidth:{xs:294,md:420},marginX:'auto'}}>
+                <Box sx={{display:'flex',flexDirection:'column',maxWidth:{xs:294,md:500},marginX:'auto'}}>
                 <CardMedia
                 component="img"
-                image={product.image_url[0]}
-                sx={{height:{xs:280,md:400},width:{xs:294,md:420},objectFit:"contain",bgcolor:"white"}}/>
+                image={image}
+                sx={{height:{xs:280,md:400},width:{xs:294,md:420},objectFit:"contain",bgcolor:"white",minHeight:{xs:280,md:400},minWidth:{xs:294,md:420}}}/>
                 <Box sx={{display:'flex',mt:1,justifyContent:'space-between'}}>
                     {product.image_url.map(e=>(
                         <CardMedia
                         component="img"
                         image={e}
-                        sx={{height:{xs:70,md:100},width:{xs:70,md:100}}}/>
+                        sx={{height:{xs:70,md:100},width:{xs:70,md:100},opacity:`${image===e||0.5}`,border:`${image===e && '4px solid'}`,borderImage:`${image===e && 'linear-gradient(to left, #00C853, #B2FF59) 1;'}`}}
+                        onClick={()=>setImage(()=>e)}/>
                     ))}
                 </Box>
                 </Box>
-                <Box sx={{display:'flex',flexDirection:'column',ml:{xs:0,md:2},alignItems:'flex-start',maxWidth:600,marginX:{xs:2}}}>
-                    <Box>
-                        <Typography sx={{color:'white',fontFamily:'ailerons',fontSize:20,display:{xs:'none',md:'block'}}}>{product.brand.name}</Typography>
+                <Box sx={{display:'flex',flexDirection:'column',ml:{xs:0,md:2},alignItems:'center',maxWidth:600,marginX:{xs:2}}}>
+                    <Box sx={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+                        <CardMedia
+                        component="img"
+                        image={product.brand.logo_url}
+                        sx={{filter: "invert(100%)",width:50,height:50}}
+                        />
+                        <Typography sx={{color:'white',fontFamily:'ailerons',fontSize:20,}}>{product.brand.name}</Typography>
                     </Box>
-                    <Typography sx={{color:'white',fontSize:{xs:35,md:50},fontWeight:700}} className='Meet'>{product.name}</Typography>
-                    <Typography sx={{color:'white',fontWeight:700,fontSize:30}} className='Meet'>${product.price}</Typography>
-                    <Box sx={{bgcolor:'white',p:3,borderRadius:3,minWidth:{xs:'50%',md:'100%'},height:'100%',boxShadow:'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;'}}>
-                        <Box sx={{display:'flex',justifyContent:'flex-start'}}>
-                            <Typography sx={{color:'black',fontWeight:500,fontSize:20}} >Product Description</Typography>
-                        </Box>
-                        <Divider/>
-                        <Box sx={{overflow:'auto',mt:2}}>
-                            <Typography minWidth='100%' textAlign="left">{product.description}</Typography>
+                    <Box  sx={{display:'flex',flexDirection:'column',ml:{xs:0,md:2},alignItems:'flex-start',maxWidth:'100%',marginX:{xs:2}}}>
+                        <Typography sx={{color:'white',fontSize:{xs:35,md:45},fontWeight:700}} className='Meet'>{product.name}</Typography>
+                        <Typography sx={{color:'white',fontWeight:700,fontSize:30}} className='Meet'>${product.price}</Typography>
+                        <Box className='fondoblanco' sx={{p:3,borderRadius:3,minWidth:{xs:'50%',md:'100%'},height:'100%',boxShadow:'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;',mt:2}}>
+                            <Box sx={{display:'flex',justifyContent:'flex-start'}}>
+                                <Typography sx={{color:'black',fontWeight:500,fontSize:20}} >Product Description</Typography>
+                            </Box>
+                            <Divider/>
+                            <Box  sx={{overflow:'auto',mt:2}}>
+                                <Typography textAlign="left">{product.description}</Typography>
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
